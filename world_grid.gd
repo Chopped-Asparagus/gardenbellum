4a8b317a81ee2f5d983e8cf0e0bef3e26f6fd24f
Eric Devenport <edeven2@wgu.edu>
2026-07-16 17:56:22 -0600

    Started ph system

--- world_grid.gd
+++ world_grid.gd
@@ -1,5 +1,6 @@
 extends TileMapLayer
 
+const HOTSPOT_CHANCE = 0.10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func generate_ph() -> void:
+	var worldArray = get_used_cells()
+	var startPos = get_used_rect().position
+	for cell in worldArray:
+		
+		pass
