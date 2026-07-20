8399a8a9adc6bb65de8566aa792b16b3cb9b6a5d
Chopped-Asparagus <ericwdev@gmail.com>
2026-07-20 17:15:46 -0600

    Added curly dock, modified character sprite flipping parameters. Started prep for antler throwing

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -6,17 +6,32 @@ const TORSO_INDEX = 1
 const ANTLER_RIGID_BODY_INDEX = 6
 const ANTLER_SUB_INDEX = 0
 const ANTLER_OFFSET = -8
+const ANTLER_THROW_DIST = 300
+const FLIP_OFFSET = 2
+const CHARACTER_TYPE = "MOOSE"
+
+var attackDisabled = false
 
 
 func _physics_process(_delta: float) -> void:
+	
+	handle_movement()
+	
+	if (CHARACTER_TYPE == "MOOSE"):
+		handle_antler_throw()
+
+func handle_movement():
 	var torso = self.get_child(TORSO_INDEX)
 	var antlerRigidBody = self.get_child(ANTLER_RIGID_BODY_INDEX)
 	var antler = antlerRigidBody.get_child(ANTLER_SUB_INDEX)
+	
+	#handle movement
 	
 	velocity.x = 0
 	velocity.y = 0
 	
 	if (Input.is_action_pressed("move_right") and !Input.is_action_pressed("move_left")):
+		torso.position.x = 0
 		torso.flip_h = false
 		antler.flip_h = false
 		antlerRigidBody.position.x = ANTLER_OFFSET
@@ -29,9 +44,10 @@ func _physics_process(_delta: float) -> void:
 		else:
 			velocity.x = SPEED
 	elif (Input.is_action_pressed("move_left") and !Input.is_action_pressed("move_right")):
+		torso.position.x = FLIP_OFFSET
 		torso.flip_h = true
 		antler.flip_h = true
-		antlerRigidBody.position.x = -ANTLER_OFFSET
+		antlerRigidBody.position.x = -ANTLER_OFFSET + FLIP_OFFSET
 		if (Input.is_action_pressed("move_up") != Input.is_action_pressed("move_down")):
 			velocity.x = -SPEED * DIAG_MOD
 			if (Input.is_action_pressed("move_up")):
@@ -44,5 +60,12 @@ func _physics_process(_delta: float) -> void:
 		velocity.y = -SPEED
 	elif (Input.is_action_pressed("move_down") and !Input.is_action_pressed("move_up")):
 		velocity.y = SPEED
-	
+		
 	move_and_slide()
+	
+func handle_antler_throw():
+	var antlerRigidBody = self.get_child(ANTLER_RIGID_BODY_INDEX)
+	var antler = antlerRigidBody.get_child(ANTLER_SUB_INDEX)
+	if (Input.is_action_just_pressed("primary_attack") && attackDisabled == false):
+		
+		pass
--- world_grid.gd
+++ world_grid.gd
@@ -1,6 +1,6 @@
 extends TileMapLayer
 
-const HOTSPOT_CHANCE = 0.10
+const HOTSPOT_RAND_NUM = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,8 +12,21 @@ func _process(delta: float) -> void:
 	pass
 	
 func generate_ph() -> void:
+	var hotspotArray: Array[Vector2i] = []
 	var worldArray = get_used_cells()
 	var startPos = get_used_rect().position
 	for cell in worldArray:
-		
-		pass
+		if (!checkNeighborHotspots(hotspotArray, cell)):
+			if (randi_range(1, HOTSPOT_RAND_NUM) == HOTSPOT_RAND_NUM):
+				hotspotArray.append(cell)
+
+func checkNeighborHotspots(hotspotArray: Array, cell: Vector2i) -> bool:
+	if (hotspotArray.has(Vector2i(cell.x - 1, cell.y))):
+		return true
+	elif (hotspotArray.has(Vector2i(cell.x + 1, cell.y))):
+		return true
+	elif (hotspotArray.has(Vector2i(cell.x, cell.y - 1))):
+		return true
+	elif (hotspotArray.has(Vector2i(cell.x, cell.y + 1))):
+		return true
+	return false
